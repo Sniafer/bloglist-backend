a5949eba77a6dc47b50c5f3de10ef3cb2d1015a4
Sniafer <wiktor.maszota@gmail.com>
2022-01-20 16:51:46 +0100

    Added posts

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,9 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const Post = require("../models/post");
 const middleware = require("../utils/middleware");
 
+//Blogs
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
@@ -59,5 +61,49 @@ blogsRouter.put("/:id", async (request, response) => {
   });
   response.status(201).json(updated);
 });
+
+//Comments
+blogsRouter.post(
+  "/:id/comments",
+  middleware.userExtractor,
+  async (request, response) => {
+    const body = request.body;
+    const blog = await Blog.findById(request.params.id);
+    const user = request.user;
+
+    const post = new Post({
+      text: body.text,
+      user: user._id,
+      blog: blog._id,
+    });
+    const savedPost = await post.save();
+
+    response.status(201).json(savedPost);
+  }
+);
+
+blogsRouter.get("/:id/comments", async (request, response) => {
+  const posts = await Post.find({ blog: { _id: request.params.id } }).populate(
+    "user",
+    { username: 1, name: 1 }
+  );
+  response.json(posts);
+});
+
+blogsRouter.delete(
+  "/:id/comments/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    const post = await Post.findById(request.params.id);
+    const user = request.user;
+
+    if (post.user.toString() === user.id.toString()) {
+      await Post.findByIdAndRemove(request.params.id);
+      response.status(204).end();
+    } else {
+      response.status(401).json({ error: "unauthorized request" });
+    }
+  }
+);
 
 module.exports = blogsRouter;
---
+++ models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  blog: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Blog",
+  },
+});
+
+postSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
